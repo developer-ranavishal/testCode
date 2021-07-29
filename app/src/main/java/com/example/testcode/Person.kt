e637b50d99ac3a6ae32feadc174afc4ff90edc13
Vishal Rana <vishalrana9038@gmail.com>
2021-07-29 11:03:08 +0530

    added two parameter singer and its rating

--- app/src/main/java/com/example/testcode/Person.kt
+++ app/src/main/java/com/example/testcode/Person.kt
@@ -4,6 +4,8 @@ data class Person(
     val name : String,
     val friendName : String,
     val surname : String,
-    val salary : Int
-    val bestFriend : String
+    val salary : Int,
+    val bestFriend : String,
+    val fav_singer : String,
+    val singer_rating : Int
     )
